7e5665d40dc99916c85b97827d40cb67361f0c01
jjohnc <43979310+jjohnc@users.noreply.github.com>
2018-10-10 12:21:38 +0530

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,13 +1,3 @@
-Python 3.7.0a4 (v3.7.0a4:07c9d85, Jan  9 2018, 07:07:02) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> def add(num1,num2):
-	
-
-
-def
-SyntaxError: expected an indented block
->>> 
->>> 
 >>> coconuts=2
 >>> print("I have", coconuts, "coconuts")
 I have 2 coconuts
