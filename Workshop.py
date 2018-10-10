2edd1669f44878fdbbda618a5e850d3821976fc6
jjohnc <43979310+jjohnc@users.noreply.github.com>
2018-10-10 13:54:55 +0530

    WORKSHOP

---
+++ Workshop.py
@@ -0,0 +1,30 @@
+import random
+import math
+i = random.randint(50, 80)
+
+if (i < 60):
+
+    print(i,  "is less than 60")
+
+elif(i > 60):
+
+    print(i,  "is greater than 60")
+
+    # TODO: Write a conditional that prints the color of the picked Animal
+picked_Animal = random.choice(['Elephant', 'Lion', 'Tiger'])
+
+if (picked_Animal == "Elephant"):
+
+    print("Elephant")
+
+elif (picked_Animal == "Lion"):
+
+    print("Lion")
+
+elif (picked_Animal == "Tiger"):
+
+    print("Tiger")
+
+          
+         
+  
