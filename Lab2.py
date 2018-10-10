e8e358b011e70441f0c1dba7d12e627060247f81
jjohnc <43979310+jjohnc@users.noreply.github.com>
2018-10-10 12:18:17 +0530

    Prog

---
+++ Lab2.py
@@ -0,0 +1,7 @@
+n=5
+if n<5:
+    print("n is less than five")
+elif n==5:
+    print("n is equal to five")
+else:
+        print("n is grater than five")
---
+++ Lab3.py
@@ -0,0 +1,6 @@
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+
+    add(4, 9)
